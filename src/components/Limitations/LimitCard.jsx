71411362b79a11adc172136e9666284191df3bac
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-06-22 18:50:25 -0400

    added some more styling

--- src/components/Limitations/LimitCard.jsx
+++ src/components/Limitations/LimitCard.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './Limitations.scss';
 
 function LimitCard({ icon, alt, title, description }) {
   return (
--- src/components/Limitations/Limitations.jsx
+++ src/components/Limitations/Limitations.jsx
@@ -9,24 +9,25 @@ import iconMuscle from '../../assets/images/icon-muscle.svg';
 import iconPreg from '../../assets/images/icon-pregnancy.svg';
 import iconRace from '../../assets/images/icon-race.svg';
 import arc from '../../assets/images/pattern-curved-line-right.svg'
+// import './Limitations.scss';
 
 function Limitations() {
   return (
     <section className='limitations'>
-    <section className='limitation__upper'>
-      <LimitationsLeft />
-      <LimitCard 
-        icon={iconGender}
-        alt={limitData[0].alt}
-        title={limitData[0].title}
-        description={limitData[0].description}
-      />
-    </section>
-    <section className='limitations__mid-section'>
-      <img src={arc} alt="curved-line" />
-      <LimitationsDouble sectionClass="limitations__middle-section" iconFirst={iconAge} iconSecond={iconMuscle} cardObjFirst={limitData[1]} cardObjSecond={limitData[2]}/>
-    </section>
-    <LimitationsDouble sectionClass="limitations__bottom-section" iconFirst={iconPreg} iconSecond={iconRace} cardObjFirst={limitData[3]} cardObjSecond={limitData[4]}/>
+      <section className='limitations__upper'>
+        <LimitationsLeft />
+        <LimitCard 
+          icon={iconGender}
+          alt={limitData[0].alt}
+          title={limitData[0].title}
+          description={limitData[0].description}
+        />
+      </section>
+      <section className='limitations__mid-section'>
+        <img className='limitations__curve' src={arc} alt="curved-line" />
+        <LimitationsDouble sectionClass="limitations__middle-section" iconFirst={iconAge} iconSecond={iconMuscle} cardObjFirst={limitData[1]} cardObjSecond={limitData[2]}/>
+      </section>
+      <LimitationsDouble sectionClass="limitations__bottom-section" iconFirst={iconPreg} iconSecond={iconRace} cardObjFirst={limitData[3]} cardObjSecond={limitData[4]}/>
     </section>
     
 
